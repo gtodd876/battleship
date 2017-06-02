76065ae273a74c1d5dbbb9cc876420ed9170ff8f
Todd Matthews <gtmatthews@gmail.com>
2017-06-02 19:30:21 -0400

    new features: lower-upper case, text field focus, enter key enters your guess

--- battleship.js
+++ battleship.js
@@ -78,7 +78,8 @@ var view = {
         alert("Oops, please enter a letter and a number on the board");
       } else {
         var firstChar = guess.charAt(0);
-        var row = alphabet.indexOf(firstChar);
+        var upperChar = firstChar.toUpperCase();
+        var row = alphabet.indexOf(upperChar);
         var column = guess.charAt(1);
         if (isNaN(row) || isNaN(column)) {
           alert ("Oops, that isn't on the board.");
@@ -94,12 +95,22 @@ var view = {
     function init() {
       var fireButton = document.getElementById("fireButton");
       fireButton.onclick = handleFireButton;
+      var guessInput = document.getElementById("guessInput");
+      guessInput.onkeypress = handleKeyPress;
     }
     function handleFireButton() {
       var guessInput = document.getElementById("guessInput");
       var guess = guessInput.value;
       controller.processGuess(guess);
       guessInput.value = "";
+      document.getElementById("guessInput").focus();
+    }
+    function handleKeyPress(e) {
+      var fireButton = document.getElementById("fireButton");
+      if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+      }
     }
     window.onload = init;
   //   Testing parseGuess
